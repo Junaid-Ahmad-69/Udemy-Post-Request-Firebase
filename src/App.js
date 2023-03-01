ca3833f7343f8f07ee4646ff912a0f2853a69194
Junaid Sandhu <junaidsandhu21@gmail.com>
2023-03-01 19:27:38 +0500

    Django restfull api to GET request & POST request with firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useCallback} from 'react';
+import MoviesList from "./Components/MoviesList";
 import './App.css';
+import AddMovie from "./Components/AddMovie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        FetchMoviesHandler();
+    }, []);
+
+    const FetchMoviesHandler = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch("https://udemy-react-http-post-request-default-rtdb.firebaseio.com/movies.json");
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+            const data = await response.json();
+
+
+            // For Post Request
+            const loadedMovies = [];
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate,
+                });
+            }
+            setMovies(loadedMovies);
+
+            // For Get Request
+            // const transformMovies = data.map((moviesData) => {
+            //     return {
+            //         id: moviesData.id,
+            //         title: moviesData.title,
+            //         openingText: moviesData.opening_crawl,
+            //         releasedDate: moviesData.release_date,
+            //     };
+            // });
+            // setMovies(transformMovies);
+        } catch (error) {
+            setError(error.message)
+        }
+        setIsLoading(false);
+    }, []);
+
+    async function addMovieHandler(movie) {
+        const response = await fetch("https://udemy-react-http-post-request-default-rtdb.firebaseio.com/movies.json", {
+            method: "POST",
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data)
+    }
+
+    let content = <p>Found no Movies.</p>
+
+    if (movies.length > 0) {
+        content = <MoviesList movies={movies}/>
+    }
+    if (error) {
+        content = <p>{error}</p>
+    }
+    if (isLoading) {
+        content = <p>.....Loading 🍭</p>
+    }
+    return (
+        <>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler}/>
+            </section>
+
+            <section>
+                <button onClick={FetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>{content}</section>
+        </>
+    );
 }
 
 export default App;
+
+// Django restfull api
+// https://swapi.dev/api/films/
---
+++ src/Components/MoviesList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Movie from './Movies';
+import classes from './MoviesList.module.css';
+
+const MovieList = (props) => {
+    return (
+        <ul className={classes['movies-list']}>
+            {props.movies.map((movie, index) => (
+                <Movie
+                    key={index}
+                    title={movie.title}
+                    releaseDate={movie.releaseDate}
+                    openingText={movie.openingText}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default MovieList;
